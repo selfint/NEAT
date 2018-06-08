bddf542d1910e5d8d69480110924f145aba60fa6
tomaster777 <selfint@gmail.com>
2018-06-08 16:06:25 +0200

    Added basic files for project

---
+++ neat.py
@@ -0,0 +1,9 @@
+# neat.py
+# 
+# Description - Builds a network that can evolve it's weights and it's topology.
+# -------------------------------------------------------------------------------
+
+# Imports
+
+# Constants
+
